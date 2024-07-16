4555365a7dfecf6ed794dcd498cd6ead58c2acfb
Aman Patidar <amanpatidar.synergytop@gmail.com>
2024-07-16 16:08:35 +0530

    work on card

--- Points.js
+++ Points.js
@@ -1,41 +1,36 @@
+let selectedCard;
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    const hoverTarget = document.querySelector('.hover-target');
-    const connectivityCard = document.getElementById('connectivityCard');
-    const toggleButton = document.getElementById('toggleButton');
-    const pointsForm = document.getElementById('pointsForm');
-    let clickCount = 0;
-
-    hoverTarget.addEventListener('click', function() {
-        clickCount++;
-        if (clickCount % 2 !== 0) {
-            connectivityCard.style.display = 'block';
-        } else {
-            connectivityCard.style.display = 'none';
-        }
-    });
-
-    hoverTarget.addEventListener('dblclick', function() {
-        connectivityCard.style.display = 'none';
-        clickCount = 0; // Reset click count on double click
-    });
-
-    toggleButton.addEventListener('click', function() {
-        if (pointsForm.style.display === 'none') {
-            pointsForm.style.display = 'block';
-        } else {
-            pointsForm.style.display = 'none';
-        }
-    });
-});
-
-function savePoints() {
+const savePoints = () => {
     const favorablePoints = document.getElementById('favorablePoints').value;
     const unfavorablePoints = document.getElementById('unfavorablePoints').value;
     alert('Favorable Points: ' + favorablePoints + '\nUnfavorable Points: ' + unfavorablePoints);
     // You can add further logic to save these points, e.g., send them to a server
 
-    const pointsForm = document.getElementById('pointsForm');
+    const pointsForm = selectedCard.querySelector('#pointsForm');
     pointsForm.style.display = 'none';
+}
+
+const openCardById = (id) => {
+    selectedCard = document.getElementById(id);
+    const pointsForm = selectedCard.querySelector('#pointsForm');
+    pointsForm.style.display = 'none';
+    let elements = document.querySelectorAll('.rating-card');
+    elements?.forEach((item) => {
+        if (item?.id === id) {
+            if (selectedCard.style.display === 'block') {
+                selectedCard.style.display = 'none';
+            } else {
+                selectedCard.style.display = 'block';
+            }
+        } else
+            item.style.display = 'none';
+    })
+}
+const openBoxInCard = () => {
+    const pointsForm = selectedCard.querySelector('#pointsForm');
+    if (pointsForm.style.display === 'block') {
+        pointsForm.style.display = 'none';
+    } else {
+        pointsForm.style.display = 'block';
+    }
 }
