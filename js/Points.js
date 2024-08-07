c435861c97ec206158c0c4ae5e00980720153228
Aman Patidar <amanpatidar.synergytop@gmail.com>
2024-08-07 19:06:08 +0530

    show rating onlcick + - buttons

--- js/Points.js
+++ js/Points.js
@@ -10,14 +10,8 @@ const savePoints = () => {
     pointsForm.style.display = 'none';
 }
 
-const openCardById = (id, type = "positive") => {
-    let cardKey = id === 'connectivityCard' ? ['connectivityLikes', 'connectivityDislikes']
-        : id === 'constructionCard' ? ['constructionLikes', 'constructionDisLikes']
-            : id === 'amenitiesCard' ? ['amenitiesLikes', 'amenitiesDisLikes']
-                : id === 'maintenanceCard' ? ['maintenanceLikes', 'maintenanceDisLikes']
-                    : id === 'peopleFriendlinessCard' ? ['peopleFriendlinessLikes', 'peopleFriendlinessDisLikes'] : '';
+const openCardById = (id) => {
     selectedCard = document.getElementById(id);
-
     let elements = document.querySelectorAll('.rating-card');
     elements?.forEach((item) => {
         if (item?.id === id) {
@@ -28,8 +22,79 @@ const openCardById = (id, type = "positive") => {
             }
         } else
             item.style.display = 'none';
-    })
+    });
+    showRatingInsideCard(id, "positive");
+}
+const openBoxInCard = () => {
+    const pointsForm = selectedCard.querySelector('#pointsForm');
+    if (pointsForm.style.display === 'block') {
+        pointsForm.style.display = 'none';
+    } else {
+        pointsForm.style.display = 'block';
+    }
+}
+
+function showPositive(id) {
+    let cardKey = id === 'connectivityCard' ? 'connectivityLikes' : id === 'constructionCard' ? 'constructionLikes' : id === 'amenitiesCard' ? 'amenitiesLikes' : id === 'maintenanceCard' ? 'maintenanceLikes' : id === 'peopleFriendlinessCard' ? 'peopleFriendlinessLikes' : '';
+    const sortedLikes = Object.entries(nlpDataForLikeDislike[cardKey])
+        .sort(([, a], [, b]) => a - b)
+        .reduce((acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+        }, {});
+    let rankList = document.getElementById(`rankList-${id}`);
+    rankList.style.display = 'block';
+    rankList.style.listStyle = 'none';
+    rankList.style.textTransform = 'uppercase';
+    rankList.style.marginTop = '20px';
+    rankList.style.fontSize = '10px';
+    rankList.style.textAlign = 'center';
+    rankList.innerHTML = '';
+    for (let i = Object.keys(sortedLikes).length - 1; i >= 0; i--) {
+        let listItem = document.createElement('li');
+        listItem.textContent = Object.keys(sortedLikes)[i];
+        let hue = 115;
+        let brightness = 30 + (5 - i) * 15; // Adjust values for gradient effect
+        listItem.style.backgroundColor = 'hsl(' + hue + ', 100%, ' + brightness + '%)';
+        let perceivedBrightness = brightness / 100 * 255;
+        listItem.style.color = perceivedBrightness > 80 ? 'black' : 'white';
+        rankList.appendChild(listItem);
+    }
+}
+
+function showNegative(id) {
+    let cardKey = id === 'connectivityCard' ? 'connectivityDislikes' : id === 'constructionCard' ? 'constructionDislikes' : id === 'amenitiesCard' ? 'amenitiesDislikes' : id === 'maintenanceCard' ? 'maintenanceDislikes' : id === 'peopleFriendlinessCard' ? 'peopleFriendlinessDislikes' : ''
+    const sortedDislikes = Object.entries(nlpDataForLikeDislike[cardKey])
+        .sort(([, a], [, b]) => a - b)
+        .reduce((acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+        }, {});
+    let rankList = document.getElementById(`rankList-${id}`);
+    rankList.style.display = 'block';
+    rankList.innerHTML = '';
+    for (var i = Object.keys(sortedDislikes).length - 1; i >= 0; i--) {
+        var listItem = document.createElement('li');
+        listItem.textContent = Object.keys(sortedDislikes)[i];
+        var hue = 0;
+        var brightness = 50 + (-i + 5) * 10; // Adjust values for gradient effect
+        listItem.style.backgroundColor = 'hsl(' + hue + ', 100%, ' + brightness + '%)';
+        let perceivedBrightness = brightness / 100 * 255;
+        listItem.style.color = perceivedBrightness > 160 ? 'black' : 'white';
+        rankList.appendChild(listItem);
+    }
+}
+
+
+const showRatingInsideCard = (id, type = "positive") => {
+    let cardKey = id === 'connectivityCard' ? ['connectivityLikes', 'connectivityDislikes']
+        : id === 'constructionCard' ? ['constructionLikes', 'constructionDisLikes']
+            : id === 'amenitiesCard' ? ['amenitiesLikes', 'amenitiesDisLikes']
+                : id === 'maintenanceCard' ? ['maintenanceLikes', 'maintenanceDisLikes']
+                    : id === 'peopleFriendlinessCard' ? ['peopleFriendlinessLikes', 'peopleFriendlinessDisLikes'] : '';
+
     if (!cardKey) return;
+    selectedCard = document.getElementById(id);
     const ulElement = selectedCard.querySelector('ul');
     ulElement.innerHTML = '';
     let indexNo = type === 'positive' ? 0 : 1;
@@ -57,87 +122,4 @@ const openCardById = (id, type = "positive") => {
 
         ulElement.appendChild(liElement);
     }
-
-}
-const openBoxInCard = () => {
-    const pointsForm = selectedCard.querySelector('#pointsForm');
-    if (pointsForm.style.display === 'block') {
-        pointsForm.style.display = 'none';
-    } else {
-        pointsForm.style.display = 'block';
-    }
-}
-
-function showPositive(id) {
-    let cardKey = id === 'connectivityCard' ? 'connectivityLikes' : id === 'constructionCard' ? 'constructionLikes' : id === 'amenitiesCard' ? 'amenitiesLikes' : id === 'maintenanceCard' ? 'maintenanceLikes' : id === 'peopleFriendlinessCard' ? 'peopleFriendlinessLikes' : '';
-    const sortedLikes = Object.entries(nlpDataForLikeDislike[cardKey])
-        .sort(([, a], [, b]) => a - b)
-        .reduce((acc, [key, value]) => {
-            acc[key] = value;
-            return acc;
-        }, {});
-    // let cardData = document.getElementById(id)
-    let rankList = document.getElementById(`rankList-${id}`);
-    // Toggle visibility of rankList
-    // if (rankList.style.display === 'none' || !rankList.style.display) {
-    rankList.style.display = 'block';
-    rankList.style.listStyle = 'none';
-    rankList.style.textTransform = 'uppercase';
-    rankList.style.marginTop = '20px';
-    rankList.style.fontSize = '10px';
-    rankList.style.textAlign = 'center';
-    // Clear previous content
-    rankList.innerHTML = '';
-    // Add positive ratings
-    for (let i = Object.keys(sortedLikes).length - 1; i >= 0; i--) {
-        let listItem = document.createElement('li');
-        listItem.textContent = Object.keys(sortedLikes)[i];
-        // listItem.textContent = '+ ' + Object.keys(sortedLikes)[i] + ' ' + Object.values(sortedLikes)[i];
-        let hue = 115;
-        // Calculate brightness based on rating (5 is darkest, 1 is lightest)
-        let brightness = 30 + (5 - i) * 15; // Adjust values for gradient effect
-        // Set background color using hsl format
-        listItem.style.backgroundColor = 'hsl(' + hue + ', 100%, ' + brightness + '%)';
-        let perceivedBrightness = brightness / 100 * 255;
-        listItem.style.color = perceivedBrightness > 80 ? 'black' : 'white';
-        rankList.appendChild(listItem);
-    }
-    // } else {
-    //     rankList.style.display = 'none'; // Hide rankList
-    // }
-}
-
-function showNegative(id) {
-    let cardKey = id === 'connectivityCard' ? 'connectivityDislikes' : id === 'constructionCard' ? 'constructionDislikes' : id === 'amenitiesCard' ? 'amenitiesDislikes' : id === 'maintenanceCard' ? 'maintenanceDislikes' : id === 'peopleFriendlinessCard' ? 'peopleFriendlinessDislikes' : ''
-    const sortedDislikes = Object.entries(nlpDataForLikeDislike[cardKey])
-        .sort(([, a], [, b]) => a - b)
-        .reduce((acc, [key, value]) => {
-            acc[key] = value;
-            return acc;
-        }, {});
-    let rankList = document.getElementById(`rankList-${id}`);
-    // Toggle visibility of rankList
-    // if (rankList.style.display === 'none' || !rankList.style.display) {
-    rankList.style.display = 'block';
-    // Clear previous content
-    rankList.innerHTML = '';
-    // Add negative ratings
-    for (var i = Object.keys(sortedDislikes).length - 1; i >= 0; i--) {
-        var listItem = document.createElement('li');
-        listItem.textContent = Object.keys(sortedDislikes)[i];
-        // listItem.textContent = '- ' + Object.keys(sortedDislikes)[i] + ' ' + Object.values(sortedDislikes)[i];
-        // Calculate hue for red (0 corresponds to red in hsl)
-        var hue = 0;
-        // Calculate brightness based on rating (-5 is darkest, -1 is lightest)
-        var brightness = 50 + (-i + 5) * 10; // Adjust values for gradient effect
-        // Set background color using hsl format
-        listItem.style.backgroundColor = 'hsl(' + hue + ', 100%, ' + brightness + '%)';
-        let perceivedBrightness = brightness / 100 * 255;
-        listItem.style.color = perceivedBrightness > 160 ? 'black' : 'white';
-        rankList.appendChild(listItem);
-    }
-    // }
-    //  else {
-    //     rankList.style.display = 'none'; // Hide rankList
-    // }
 }
